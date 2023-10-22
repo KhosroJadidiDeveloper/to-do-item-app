d0284ffbe6cb00178a7a452957c2855361baf873
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2023-10-23 00:58:26 +0200

    add documentation to IUserInputService

--- ToDoApp/Application/IUserInputService.cs
+++ ToDoApp/Application/IUserInputService.cs
@@ -1,7 +1,19 @@
 namespace ToDoApp.Application;
 
+/// <summary>
+/// Interface for a service that provides user input functionality.
+/// </summary>
 public interface IUserInputService
 {
+    /// <summary>
+    /// Gets a string input from the user.
+    /// </summary>
+    /// <returns>The string input provided by the user.</returns>
     string GetStringInput();
+
+    /// <summary>
+    /// Gets a numeric input from the user.
+    /// </summary>
+    /// <returns>The numeric input provided by the user.</returns>
     int GetNumericInput();
 }
