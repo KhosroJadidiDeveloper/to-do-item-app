373cc50d9dc2813dbe429e24bd436cfa45e288f8
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2023-10-23 01:08:32 +0200

    add documentation to IConsoleDisplay

--- ToDoApp/Presentation/IConsoleDisplay.cs
+++ ToDoApp/Presentation/IConsoleDisplay.cs
@@ -1,9 +1,31 @@
 namespace ToDoApp.Presentation;
 
+/// <summary>
+/// Interface for displaying messages on the console with different colors based on the message type.
+/// </summary>
 public interface IConsoleDisplay
 {
+    /// <summary>
+    /// Displays an informational message in white color.
+    /// </summary>
+    /// <param name="message">The message to display.</param>
     void DisplayInfo(string message);
+
+    /// <summary>
+    /// Displays an error message in red color.
+    /// </summary>
+    /// <param name="message">The message to display.</param>
     void DisplayError(string message);
+
+    /// <summary>
+    /// Displays a success message in green color.
+    /// </summary>
+    /// <param name="message">The message to display.</param>
     void DisplaySuccess(string message);
+
+    /// <summary>
+    /// Displays a warning message in yellow color.
+    /// </summary>
+    /// <param name="message">The message to display.</param>
     void DisplayWarning(string message);
 }
