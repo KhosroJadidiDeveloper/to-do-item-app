40ad2eca4385a511dfe5bd03987bb61004a6c32c
Khosro Jadidi <khosro.jadidi.developer@gmail.com>
2023-10-23 00:56:19 +0200

    add documentation to ToDoItem

--- ToDoApp/Domain/ToDoItem.cs
+++ ToDoApp/Domain/ToDoItem.cs
@@ -1,9 +1,27 @@
 namespace ToDoApp.Domain;
 
+/// <summary>
+/// Represents a to-do item.
+/// </summary>
 public class ToDoItem
 {
+    /// <summary>
+    /// Gets or sets the unique identifier of the to-do item.
+    /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the title of the to-do item.
+    /// </summary>
     public string? Title { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description of the to-do item.
+    /// </summary>
     public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the to-do item is done.
+    /// </summary>
     public bool IsDone { get; set; }
 }
